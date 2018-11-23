17557c64a66acc5decfb4c5692d824326fc78320
nignanthomas <nignanthomas@gmail.com>
2018-11-23 11:10:52 +0300

    create folder structure; create application module flaskblog.py; templates home.html-about.html-layout.html(as base for html structure)

---
+++ flaskblog.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,url_for
+app = Flask(__name__)
+
+
+
+
+
+
+posts = [
+    {
+        'author': 'Nie Senfan',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'November 22, 2018'
+    },
+    {
+        'author': 'Nyakio Mburu',
+        'title': 'Blog Post 2',
+        'content': 'Seconf pst content',
+        'date_posted': 'November 25, 2018'
+    }
+
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
