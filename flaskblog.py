3a1ce8bf4265f0a3aa93264a427d00c4f3a94e00
nignanthomas <nignanthomas@gmail.com>
2018-11-23 20:35:00 +0300

    add content to  login.html and update login view function

--- flaskblog.py
+++ flaskblog.py
@@ -44,9 +44,17 @@ def register():
     return render_template('register.html', form = form, title = 'Flask Blog -- Register')
 
 
-@app.route('/login')
+@app.route('/login', methods =['GET','POST'])
+def register():)
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
 
     return render_template('login.html', form = form, title = 'Flask Blog -- Login')
 
