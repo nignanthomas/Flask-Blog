6460b6538bc17a8ed933d961dc1c9e99e32cb064
nignanthomas <nignanthomas@gmail.com>
2018-11-25 21:31:16 +0300

    add create/update/delete posts feature

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,9 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template,url_for,flash,redirect,request
+from flask import render_template,url_for,flash,redirect,request,abort
 from flaskblog import app,db,bcrypt
-from flaskblog.forms import RegistrationForm,LoginForm,UpdateAccountForm
+from flaskblog.forms import RegistrationForm,LoginForm,UpdateAccountForm,PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user,current_user,logout_user,login_required
 
@@ -12,25 +12,11 @@ from flask_login import login_user,current_user,logout_user,login_required
 
 
 
-posts = [
-    {
-        'author': 'Nie Senfan',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'November 22, 2018'
-    },
-    {
-        'author': 'Nyakio Mburu',
-        'title': 'Blog Post 2',
-        'content': 'Seconf pst content',
-        'date_posted': 'November 25, 2018'
-    }
-
-]
-
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
+
     return render_template('home.html', posts = posts)
 
 
@@ -116,3 +102,68 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename = 'photos/'+current_user.image_file)
     return render_template('account.html', form = form, title = 'Flask Blog -- My Account', image_file = image_file)
+
+
+
+
+################################################################################
+
+
+@app.route('/post/new', methods =['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Post(title = form.title.data, content = form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+
+        flash('Your post has been submitted!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', form = form, title = 'Flask Blog -- New Post', legend = 'New Post')
+
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    return render_template('post.html', title = 'Flask Blog -- '+post.title, post = post)
+
+
+
+
+@app.route('/post/<int:post_id>/update', methods =['GET','POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('create_post.html', form = form, title = 'Flask Blog -- Update('+post.title+')', post = post, legend = 'Update Post')
+
+
+
+@app.route('/post/<int:post_id>/delete', methods =['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('The post has been deleted!', 'success')
+    return redirect(url_for('home'))
